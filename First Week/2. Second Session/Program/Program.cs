58c0a8eb1a7fd36edb8c883b1fe79e40c230ed62
Abdelwahab Ayman <abdelwahab.aym.aboels3od@gmail.com>
2026-07-28 20:08:22 +0300

    Initial commit

---
+++ First Week/2. Second Session/Program/Program.cs
@@ -0,0 +1,41 @@
+﻿//Console.WriteLine("Hello, World!");
+//int age = 30;
+//string name = "John Doe";
+//Concatenation not recommended
+//Console.WriteLine("my name is " + name + " and I am " + age + " years old.");
+//String interpolation recommended
+//Console.WriteLine($"my name is {name} and I am {age} years old.");
+
+//#region Read From Console
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        #region Read From Console
+        int parsedAge;   //4B
+        string userName;
+        double salary; //8B
+
+        Console.WriteLine("Enter your age: ");
+
+        parsedAge = int.Parse(s: Console.ReadLine());
+
+        //parsedAge = parsedAge;
+
+        Console.WriteLine("Enter your name: ");
+        userName = Console.ReadLine();
+
+        Console.WriteLine("Enter your salary: ");
+        salary = double.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Your name is {userName}, you are {parsedAge} years old and your salary is {salary}.");
+        #endregion
+
+        #region urinary operators
+
+
+        #endregion
+
+    }
+}
