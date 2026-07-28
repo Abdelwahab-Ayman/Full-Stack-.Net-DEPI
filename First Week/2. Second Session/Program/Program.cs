b7f1e5ef83296abacff5d40df51d8d9c95b90ae1
Abdelwahab Ayman <abdelwahab.aym.aboels3od@gmail.com>
2026-07-28 20:55:49 +0300

    update conditional statment

--- First Week/2. Second Session/Program/Program.cs
+++ First Week/2. Second Session/Program/Program.cs
@@ -12,44 +12,61 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        #region Read From Console
-        int parsedAge;   //4B
-        string userName;
-        double salary; //8B
+        //#region Read From Console
+        //int parsedAge;   //4B
+        //string userName;
+        //double salary; //8B
 
-        Console.WriteLine("Enter your age: ");
+        //Console.WriteLine("Enter your age: ");
 
-        parsedAge = int.Parse(s: Console.ReadLine());
+        //parsedAge = int.Parse(s: Console.ReadLine());
 
         //parsedAge = parsedAge;
 
-        Console.WriteLine("Enter your name: ");
-        userName = Console.ReadLine();
+        //Console.WriteLine("Enter your name: ");
+        //userName = Console.ReadLine();
 
-        Console.WriteLine("Enter your salary: ");
-        salary = double.Parse(Console.ReadLine());
+        //Console.WriteLine("Enter your salary: ");
+        //salary = double.Parse(Console.ReadLine());
 
-        Console.WriteLine($"Your name is {userName}, you are {parsedAge} years old and your salary is {salary}.");
-        #endregion
-
-        #region urinary operators
-
-
-        #endregion
-        #region logical operators && , ||
-
-
-
-        #endregion
-        #region ternary operator
-        int x = 10;
-        int y = 20;
+        //Console.WriteLine($"Your name is {userName}, you are {parsedAge} years old and your salary is {salary}.");
+        //#endregion
+        //#region urinary operators
+        //#endregion
+        //#region logical operators && , ||
+        //#endregion
+        //#region ternary operator
+        //int x = 10;
+        //int y = 20;
         //condition ? true : false
+        //int z = y > x ? y : x;
+        //Console.WriteLine(z);
+        //#endregion
+        #region conditional statements
+        int grade;
+        Console.WriteLine("Enter your grade: ");
 
-        int z = y > x ? y : x;
-        Console.WriteLine(z);
+        grade = int.Parse(Console.ReadLine());
+        if(grade >= 85)
+        {
+            Console.WriteLine("You got an A");
 
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine("You got a B");
+        }
+        else if (grade >= 60)
+        {
+            Console.WriteLine("You got a C");
+        }
+        else if (grade >= 50)
+        {
+            Console.WriteLine("You got a D");
+        }
+        else
+        {
+            Console.WriteLine("You failed");
+        }
         #endregion
-
     }
-}
--- First Week/2. Second Session/Program/obj/Debug/net10.0/Program.AssemblyInfo.cs
+++ First Week/2. Second Session/Program/obj/Debug/net10.0/Program.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Program")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58c0a8eb1a7fd36edb8c883b1fe79e40c230ed62")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0dfdc1818483b425a986717ff8dee4271a7ce8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Program")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Program")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
